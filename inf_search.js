2fe192fe66ecdc8073890fd560365bb610f70aee
김명주 <mjoo1106@khu.ac.kr>
2021-11-04 22:11:54 +0900

    Update detail information

--- inf_search.js
+++ inf_search.js
@@ -5,6 +5,7 @@ var app=express();
 const bodyParser=require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));  // URL 인코딩 안함 
 app.use(bodyParser.json());                         // json 타입으로 파싱하게 설정
+app.use('/inf/result',express.static('metadata'));
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -25,8 +26,37 @@ app.get("/inf/result",(req,res)=>{
     request.get(options, function(error, response, body){
         if (!error && response.statusCode == 200) {
             var jsbody=JSON.parse(body);
-            //res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'}); and res.end(body)
-            res.render('inf_search_result', {aid:jsbody.accessId, name: jsbody.name, level: jsbody.level});
+            var option2={
+                url: "https://api.nexon.co.kr/kart/v1.0/users/"+jsbody.accessId+"/matches?start_date=&end_date= &offset=0&limit=1&match_types=",
+                headers:{Authorization: key}
+            };
+            request.get(option2, function(error, response, body){
+                var match=JSON.parse(body);
+                var tId=match.matches[0].matches[0].trackId;
+                var kartId=match.matches[0].matches[0].player.kart;
+                var mrank=match.matches[0].matches[0].player.matchRank;
+                //한화라고 검색하면 아직 오류존재
+                var players=match.matches[0].matches[0].playerCount;        
+                var stime=new Date(match.matches[0].matches[0].startTime.split('T')[0]+" "+match.matches[0].matches[0].startTime.split('T')[1]);
+                var etime=new Date(match.matches[0].matches[0].endTime.split('T')[0]+" "+match.matches[0].matches[0].endTime.split('T')[1]);
+                var diff=etime-stime
+                var minute= Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+                var second = Math.floor((diff % (1000 * 60)) / 1000);
+                var diff_time=minute + " : " + second;
+                if(mrank=="99"){
+                    mrank="retire";   
+                    diff_time="retire";
+                }
+                var ranking=mrank+" / "+players
+                
+                if (!error && response.statusCode == 200) {
+                    res.render('inf_search_result', {name: jsbody.name, level: jsbody.level, trackId: tId, kart:kartId,rank:ranking,time: diff_time});
+                }
+                else{
+                    res.status(response.statusCode).end();
+                    console.log('error = ' + response.statusCode);
+                }     
+            });
           } else {
             res.status(response.statusCode).end();
             console.log('error = ' + response.statusCode);
@@ -38,6 +68,6 @@ app.get("/inf",(req,res)=>{
     res.render('inf_search_home');
 });
 
-app.listen(8080,function(req,res){
+app.listen("8080",function(req,res){
     console.log('server listening at port no. 8080');
 });
